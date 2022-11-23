2013b829d94fcf61e79c8dc0da93dfe23da02122
alex <alex.giaffreda7@gmail.com>
2022-11-23 18:17:16 +0100

    feat: terzo esercizio

---
+++ Esercizi js/esercizio_3_js/exercise-3.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+  return a + b;
+}
+
+const subtract = (a, b) => {
+  return a - b;
+}
+
+const multiply = (a, b) => {
+  return a * b;
+}
+
+const divide = (a, b) => {
+  return a / b;
+}
+
+const log = (value) => {
+  console.log(value);
+}
+
+let value = divide(subtract(multiply(sum(2, 4), sum(5, 2)), 2), 5);
+log(value);
