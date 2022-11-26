8f00ecc79aee08d373c4128d4f0e8a82b6e2a9a2
alex <alex.giaffreda7@gmail.com>
2022-11-26 11:00:47 +0100

    esercizio 19

---
+++ Esercizi js/esercizio_19_js/exercise-7.js
@@ -0,0 +1,41 @@
+class Person{
+  #firstName;
+  #lastName;
+  #age;
+  constructor (firstName, lastName, age){
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+    this.#age = age;
+}
+
+  set firstName(name){
+    this.#firstName = name; 
+} 
+  set lastName(surname){
+    this.#lastName = surname; 
+} 
+  set age(age){
+    this.#age = age; 
+}
+
+  get firstName(){
+    return this.#firstName; 
+}
+  get lastName(){
+    return this.#lastName; 
+}
+  get age(){
+    return this.#age; 
+}
+  
+  get fullName(){
+    return this.#firstName + " " + this.#lastName;
+  };
+}
+
+const person = new Person('Mario', 'Rossi', 25);
+console.log(person.fullName);
+
+person.firstName = 'Maria';
+person.lastName = 'Verdi';
+console.log(person.fullName);
