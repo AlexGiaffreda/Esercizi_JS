ab0dc663093890959dbc31521d35fd6af073eb0d
alex <alex.giaffreda7@gmail.com>
2022-12-04 12:26:23 +0100

    esercizio 37

---
+++ Esercizi js/esercizio_37_js/exercise.js
@@ -0,0 +1,15 @@
+const number = 15;
+
+let p = new Promise((resolve, reject) => {
+    if (number > 10) {
+        resolve("done")
+    }
+    else {
+        reject("undone")
+    }
+});
+p.then((print) => {
+    console.log(print)
+}).catch((error) => {
+    console.log(error)
+})
